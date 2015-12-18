393bce3585a7bee468f733306733c79a87823cdb
Cassius John-Adams <CASSIUS.JOHN-ADAMS@cbc.ca>
2015-12-17 20:25:15 -0500

    streamlining the way that we do measurement conversions

--- StarTographyDemo/Assets/Scripts/Star Data/StarData.cs
+++ StarTographyDemo/Assets/Scripts/Star Data/StarData.cs
@@ -15,16 +15,14 @@ public class StarData : StarDataFunctions {
 
 	// Functions are called from StarDataFunctions.cs
 	void Start() {
-		double lightYears = parsecToLightYear (parsecDistance);		// Parsecs to Lightyears
-		Debug.Log (lightYears + " Light Years");
+		double conversion = conDis (parsecDistance, "PA", "LD");		// Parsecs to Lightyears
+		Debug.Log ("There are "+ parsecDistance + " Parsecs in "+conversion+" Light Decades");
 		double dec = dmsToDeg (declination);							// degrees, (arc)minutes, (arc)seconds
 		Debug.Log (dec + " degrees declination");
 		double ra = hmsToDeg (rightAscention);						// Hours, Minutes, Seconds
 		Debug.Log(ra+" degrees Right Ascension");
-		double mkm = jlyToMkm (lightYears);							// Julian Light Years to Millions of Kilometers
+		double mkm = conDis (parsecDistance, "LY", "MK");							// Julian Light Years to Millions of Kilometers
 		Debug.Log (mkm + " x 1,000,000 km per Julian Light Year");
-		double kms = jlyToKms (lightYears);							// Julian Light Years to Kilometers
-		Debug.Log (kms + " km per Julian Light Year");
 
 		double angularDist = getAngDis (hmsToDeg (rightAscention), dmsToDeg (declination), 15d, 100d);			// ra1 (right ascension), dec1 (declination), ra2, dec2
 		Debug.Log (angularDist + " angular distance");
--- StarTographyDemo/Assets/Scripts/Star Data/StarDataFunctions.cs
+++ StarTographyDemo/Assets/Scripts/Star Data/StarDataFunctions.cs
@@ -71,81 +71,129 @@ public class StarDataFunctions : MonoBehaviour {
 		return result;
 	}
 
+
+	// Predefine these as it may cache or keep them in memory instead of assigning on each function call
+	double MK = 1000000d;
+	double AU = 149597870.7d;
+	double LH = 1079252848.8d;
+	double Ld = 25902068371d;
+	double LY = 9460730472600d;
+	double PA = 30856740080213.256d;
+	double LD = 94607304725808d;
+	double LC = 946073047258080d;
+	double LM = 9460730472580800d;
+
 	protected double conDis(double value, string from, string to) {
 		double ratio = 0d;
+		// Set up an array of all the possible measurement type shortforms
+		string[] inputs = new string[] {"MK", "AU", "LH", "Ld", "LY", "PA", "LD", "LC", "LM" };
+		// Set up an array of all the possible measurement variables
+		double[] measurements = new double[] {MK, AU, LH, Ld, LY, PA, LD, LC, LM };
+		// Iterate through each of the types of measurements looking for the one stored in the "from" variable
+		for (int i=0; i<measurements.Length; i++) {
+			if(inputs[i] == from) {
+				// Iterate through each type of measurement again, now looking for the one stored in the "to" variable
+				for (int a=0;a<inputs.Length;a++) {
+					if(inputs[a] == to) {
+						// We have the "from" and "to" values, so calculate them to get the ratio
+						ratio = measurements[i]/measurements[a];
+					}
+				}
+			}
+		}
+		/*
 		if (from == "MK") {
-			if(to == "AU") ratio = 1/2;
-			else if(to == "LH") ratio = 1/2;
-			else if(to == "Ld") ratio = 1/2;
-			else if(to == "LY") ratio = 1/2;
-			else if(to == "LD") ratio = 1/2;
-			else if(to == "LC") ratio = 1/2;
-			else if(to == "LM") ratio = 1/2;
+			if(to == "AU") ratio = MK/AU;
+			else if(to == "LH") ratio = MK/LH;
+			else if(to == "Ld") ratio = MK/Ld;
+			else if(to == "LY") ratio = MK/LY;
+			else if(to == "PA") ratio = MK/PA;
+			else if(to == "LD") ratio = MK/LD;
+			else if(to == "LC") ratio = MK/LC;
+			else if(to == "LM") ratio = MK/LM;
 			else Debug.LogError("You have passed an invalid 'to' string in your conDis function call");
 		} else if (from == "AU") {
-			if(to == "MK") ratio = 1/2;
-			else if(to == "LH") ratio = 1/2;
-			else if(to == "Ld") ratio = 1/2;
-			else if(to == "LY") ratio = 1/2;
-			else if(to == "LD") ratio = 1/2;
-			else if(to == "LC") ratio = 1/2;
-			else if(to == "LM") ratio = 1/2;
+			if(to == "MK") ratio = MK/MK;
+			else if(to == "LH") ratio = AU/LH;
+			else if(to == "Ld") ratio = AU/Ld;
+			else if(to == "LY") ratio = AU/LY;
+			else if(to == "PA") ratio = AU/PA;
+			else if(to == "LD") ratio = AU/LD;
+			else if(to == "LC") ratio = AU/LC;
+			else if(to == "LM") ratio = AU/LM;
 			else Debug.LogError("You have passed an invalid 'to' string in your conDis function call");
 		} else if (from == "LH") {
-			if(to == "MK") ratio = 1/2;
-			else if(to == "AU") ratio = 1/2;
-			else if(to == "Ld") ratio = 1/2;
-			else if(to == "LY") ratio = 1/2;
-			else if(to == "LD") ratio = 1/2;
-			else if(to == "LC") ratio = 1/2;
-			else if(to == "LM") ratio = 1/2;
+			if(to == "MK") ratio = LH/MK;
+			else if(to == "AU") ratio = LH/AU;
+			else if(to == "Ld") ratio = LH/Ld;
+			else if(to == "LY") ratio = LH/LY;
+			else if(to == "PA") ratio = LH/PA;
+			else if(to == "LD") ratio = LH/LD;
+			else if(to == "LC") ratio = LH/LC;
+			else if(to == "LM") ratio = LH/LM;
 			else Debug.LogError("You have passed an invalid 'to' string in your conDis function call");
 		} else if (from == "Ld") {
-			if(to == "MK") ratio = 1/2;
-			else if(to == "AU") ratio = 1/2;
-			else if(to == "LH") ratio = 1/2;
-			else if(to == "LY") ratio = 1/2;
-			else if(to == "LD") ratio = 1/2;
-			else if(to == "LC") ratio = 1/2;
-			else if(to == "LM") ratio = 1/2;
+			if(to == "MK") ratio = Ld/MK;
+			else if(to == "AU") ratio = Ld/AU;
+			else if(to == "LH") ratio = Ld/LH;
+			else if(to == "LY") ratio = Ld/LY;
+			else if(to == "PA") ratio = Ld/PA;
+			else if(to == "LD") ratio = Ld/LD;
+			else if(to == "LC") ratio = Ld/LC;
+			else if(to == "LM") ratio = Ld/LM;
 			else Debug.LogError("You have passed an invalid 'to' string in your conDis function call");
 		} else if (from == "LY") {
-			if(to == "MK") ratio = 1/2;
-			else if(to == "AU") ratio = 1/2;
-			else if(to == "LH") ratio = 1/2;
-			else if(to == "Ld") ratio = 1/2;
-			else if(to == "LD") ratio = 1/2;
-			else if(to == "LC") ratio = 1/2;
-			else if(to == "LM") ratio = 1/2;
+			if(to == "MK") ratio = LY/MK;
+			else if(to == "AU") ratio = LY/AU;
+			else if(to == "LH") ratio = LY/LH;
+			else if(to == "Ld") ratio = LY/Ld;
+			else if(to == "LD") ratio = LY/LD;
+			else if(to == "PA") ratio = LY/PA;
+			else if(to == "LC") ratio = LY/LC;
+			else if(to == "LM") ratio = LY/LM;
+			else Debug.LogError("You have passed an invalid 'to' string in your conDis function call");
+		} else if (from == "PA") {
+			if(to == "MK") ratio = PA/MK;
+			else if(to == "AU") ratio = PA/AU;
+			else if(to == "LH") ratio = PA/LH;
+			else if(to == "LY") ratio = PA/LY;
+			else if(to == "Ld") ratio = PA/Ld;
+			else if(to == "LD") ratio = PA/LD;
+			else if(to == "LC") ratio = PA/LC;
+			else if(to == "LM") ratio = PA/LM;
 			else Debug.LogError("You have passed an invalid 'to' string in your conDis function call");
 		} else if (from == "LD") {
-			if(to == "MK") ratio = 1/2;
-			else if(to == "AU") ratio = 1/2;
-			else if(to == "LH") ratio = 1/2;
-			else if(to == "Ld") ratio = 1/2;
-			else if(to == "LY") ratio = 1/2;
-			else if(to == "LC") ratio = 1/2;
-			else if(to == "LM") ratio = 1/2;
+			if(to == "MK") ratio = LD/MK;
+			else if(to == "AU") ratio = LD/AU;
+			else if(to == "LH") ratio = LD/LH;
+			else if(to == "Ld") ratio = LD/Ld;
+			else if(to == "LY") ratio = LD/LY;
+			else if(to == "PA") ratio = LD/PA;
+			else if(to == "LC") ratio = LD/LC;
+			else if(to == "LM") ratio = LD/LM;
 			else Debug.LogError("You have passed an invalid 'to' string in your conDis function call");
 		} else if (from == "LC") {
-			if(to == "MK") ratio = 1/2;
-			else if(to == "AU") ratio = 1/2;
-			else if(to == "LH") ratio = 1/2;
-			else if(to == "Ld") ratio = 1/2;
-			else if(to == "LY") ratio = 1/2;
-			else if(to == "LD") ratio = 1/2;
-			else if(to == "LM") ratio = 1/2;
+			if(to == "MK") ratio = LC/MK;
+			else if(to == "AU") ratio = LC/AU;
+			else if(to == "LH") ratio = LC/LH;
+			else if(to == "Ld") ratio = LC/Ld;
+			else if(to == "LY") ratio = LC/LY;
+			else if(to == "PA") ratio = LC/PA;
+			else if(to == "LD") ratio = LC/LD;
+			else if(to == "LM") ratio = LC/LM;
 			else Debug.LogError("You have passed an invalid 'to' string in your conDis function call");
 		} else if (from == "LM") {
-			if(to == "MK") ratio = 1/2;
-			else if(to == "AU") ratio = 1/2;
-			else if(to == "LH") ratio = 1/2;
-			else if(to == "Ld") ratio = 1/2;
-			else if(to == "LY") ratio = 1/2;
-			else if(to == "LD") ratio = 1/2;
-			else if(to == "LC") ratio = 1/2;
+			if(to == "MK") ratio = LM/MK;
+			else if(to == "AU") ratio = LM/AU;
+			else if(to == "LH") ratio = LM/LH;
+			else if(to == "Ld") ratio = LM/Ld;
+			else if(to == "LY") ratio = LM/LY;
+			else if(to == "PA") ratio = LM/PA;
+			else if(to == "LD") ratio = LM/LD;
+			else if(to == "LC") ratio = LM/LC;
 			else Debug.LogError("You have passed an invalid 'to' string in your conDis function call");
 		}
+		*/
 		double result = value * ratio;
 		return result;
 	}
