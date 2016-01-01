48cba80e269608ce156aa116eb0d97bf501a6ee3
Cassius John-Adams <CASSIUS.JOHN-ADAMS@cbc.ca>
2016-01-01 17:59:48 -0500

    Added some colliders for distance-based camera speed changes.  Complications with celestial body colliders colliding with each other

---
+++ StarTographyDemo/Assets/Scripts/Temp/collidertest.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class collidertest : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter(Collider other) {
+		Debug.LogError ("Collision Occurred between "+other.name+" and "+gameObject.name);
+	}
+	
+	
+	void OnTriggerExit(Collider other) {
+		Debug.LogError ("Collision Stopped between "+other.name+" and "+gameObject.name);
+	}
+}
