99fd6f608afc80dbff6136725ff8ab1fadca696c
Cassius John-Adams <CASSIUS.JOHN-ADAMS@cbc.ca>
2016-03-27 11:08:46 -0400

    Added in moons sub-arrays to the Format Import Data json parser for Issue #45

--- StarTographyDemo/Assets/Scripts/Setup/FormatImportData.cs
+++ StarTographyDemo/Assets/Scripts/Setup/FormatImportData.cs
@@ -1,23 +1,48 @@
-﻿using UnityEngine;
+﻿/*
+ * This script is what takes the data from a JSON formatted text file
+ * and makes sense of the data.  It uses the JsonSerializer.cs script
+ * to perform the operations.
+ * 
+ * When referring to the flowchart in Issue #45, this script represents
+ * the "Import Data" phase.
+ * 
+ * In general, first we build an array of stars, which contains a
+ * child array of planets, which contains a child array of moons.
+ * 
+ * It is assumed that the JSON contents have already made sure we
+ * don't have duplicates and that solar systems with multiple planets
+ * already have those planets set into an array for their parent star.
+ * 
+ * It's likely that, for moons, we'll only need this for the Earth
+ * solar system for the next number of years.
+ * 
+ */
+
+using UnityEngine;
 using System.Collections;
 using System.Text;
 using System.IO;
 using JSON;
 
-public class FormatImportData : MonoBehaviour
-{
-	[SerializeField] private Stars stars = null;
+public class FormatImportData : MonoBehaviour {
+	[SerializeField] private CelestialBodies celestialBodies = null;
+
 	private void Start () {
 		string json = File.ReadAllText(Path.Combine(Application.dataPath, "Scripts/config.json"));
-		this.stars = (Stars)JSONSerialize.Deserialize(typeof(Stars), json);
-		//Debug.Log (star);
+		this.celestialBodies = (CelestialBodies)JSONSerialize.Deserialize(typeof(CelestialBodies), json);
+		//Debug.Log (celestialBodies);
 
 	}
+
 }
 
 [System.Serializable]
-public class Stars {
-	
+public class CelestialBodies {
+	/*
+	 * Create an array of type Star.  This will
+	 * hold onto a list of stars that will be input via
+	 * text file from the StarTography website.
+	 */
 	[JSONArray("star", typeof(Star))]
 	public Star[] star;
 }
@@ -54,8 +79,12 @@ public class Star {
 	
 	[JSONItem("dateLastUpdate", typeof(string))]
 	public string dateLastUpdate;
-	
-	
+
+	/*
+	 * Create an array of type Planet.  This will
+	 * hold onto a list of planets that will be input via
+	 * text file for a particular parent star.
+	 */
 	[JSONArray("planets", typeof(Planet))]
 	public Planet[] planets;
 }
@@ -65,29 +94,69 @@ public class Planet {
 	[HideInInspector]
 	[JSONItem("name", typeof(string))]
 	public string name = null;
-
+	
 	[JSONItem("status", typeof(bool))]
 	public bool status = true;
-
+	
 	[JSONItem("numPlanetsInSystem", typeof(int))]
 	public int numPlanetsInSystem;
-
+	
 	[JSONItem("orbitalPeriod", typeof(float))]
 	public float orbitalPeriod;
-
+	
 	[JSONItem("semiMajorAxis", typeof(float))]
 	public float semiMajorAxis;
-
+	
 	[JSONItem("eccentricity", typeof(float))]
 	public float eccentricity;
-
+	
 	[JSONItem("inclination", typeof(float))]
 	public float inclination;
-
+	
 	[JSONItem("planetMass", typeof(float))]
 	public float planetMass;
-
+	
 	[JSONItem("planetRadius", typeof(float))]
 	public float planetRadius;
 
+	/*
+	 * Create an array of type Moon.  This will
+	 * hold onto a list of moons that will be input via
+	 * text file for a particular parent planet.
+	 */
+	[JSONArray("moons", typeof(Moon))]
+	public Moon[] moons;
+	
+}
+
+[System.Serializable]
+public class Moon {
+	[HideInInspector]
+	[JSONItem("name", typeof(string))]
+	public string name = null;
+	
+	[JSONItem("status", typeof(bool))]
+	public bool status = true;
+	
+	[JSONItem("numMoonsInSystem", typeof(int))]
+	public int numMoonsInSystem;
+	
+	[JSONItem("orbitalPeriod", typeof(float))]
+	public float orbitalPeriod;
+	
+	[JSONItem("semiMajorAxis", typeof(float))]
+	public float semiMajorAxis;
+	
+	[JSONItem("eccentricity", typeof(float))]
+	public float eccentricity;
+	
+	[JSONItem("inclination", typeof(float))]
+	public float inclination;
+	
+	[JSONItem("moonMass", typeof(float))]
+	public float moonMass;
+	
+	[JSONItem("moonRadius", typeof(float))]
+	public float moonRadius;
+	
 }
