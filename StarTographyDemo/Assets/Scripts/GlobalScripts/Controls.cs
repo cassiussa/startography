a657addd56e6a65cac6360353f23bfe0d3751352
Cassius John-Adams <CASSIUS.JOHN-ADAMS@cbc.ca>
2016-04-03 14:10:33 -0400

    Added functionality to add in the CelestialSphere with it's material and mesh.  Issue #45

---
+++ StarTographyDemo/Assets/Scripts/GlobalScripts/Controls.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Controls : MonoBehaviour {
+	public double speed = 10.0;
+	public SimulationSpeed simulationSpeed;
+	public bool enableScroll = false;
+	void Start() {
+		simulationSpeed = GetComponent<SimulationSpeed> ();
+
+	}
+
+	void Update() {
+		if (simulationSpeed.simulationSpeed < -1000000) {
+			speed = 10000000;
+		} else if (simulationSpeed.simulationSpeed < -100000) {
+			speed = 1000000;
+		} else if (simulationSpeed.simulationSpeed < -50000) {
+			speed = 100000;
+		} else if (simulationSpeed.simulationSpeed < -500) {
+			speed = 10000;
+		} else if (simulationSpeed.simulationSpeed < -10) {
+			speed = 500;
+		} else if (simulationSpeed.simulationSpeed < 50 && simulationSpeed.simulationSpeed > -50) {
+			speed = 50;
+		} else if (simulationSpeed.simulationSpeed < 500) {
+			speed = 500;
+		} else if (simulationSpeed.simulationSpeed < 10000) {
+			speed = 10000;
+		} else if (simulationSpeed.simulationSpeed < 100000) {
+			speed = 100000;
+		} else if (simulationSpeed.simulationSpeed < 1000000) {
+			speed = 1000000;
+		} else {
+			speed = 10000000;
+		}
+		if (enableScroll == true) {
+			double timeSpeed = Input.GetAxis ("Mouse ScrollWheel") * speed;
+			timeSpeed *= Time.deltaTime;
+			//simulationSpeed.simulationSpeed = Mathf.Clamp (simulationSpeed.simulationSpeed + timeSpeed, 1, 30000000);
+			simulationSpeed.simulationSpeed = simulationSpeed.simulationSpeed + timeSpeed;
+		}
+	}
+}
---
+++ StarTographyDemo/Assets/Scripts/GlobalScripts/DateTime.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DateTime : MonoBehaviour {
+	public System.DateTime epochStart = new System.DateTime(1970, 1, 1, 0, 0, 0, System.DateTimeKind.Utc);
+	public System.DateTime currentDate;
+	public int timestamp = 0;
+	public System.DateTime dtDateTime;
+	public string[] months;
+
+	public SimulationSpeed simulationSpeed;
+	double timestampUpdate = 0;
+
+	// Use this for initialization
+	void Start () {
+		timestamp = (int)(System.DateTime.UtcNow - epochStart).TotalSeconds;		// Create the unix timestamp so we can update date/time based on seconds
+		//Debug.Log ("timestamp = " + timestamp);
+		currentDate = epochStart.AddSeconds( timestamp ).ToLocalTime();				// Convert unix timestamp to date/time format
+		//Debug.Log ("dtDateTime = " + months[int.Parse(currentDate.ToString("MM"))-1]+" "+currentDate.ToString("dd, yyyy"));
+
+		simulationSpeed = GetComponent<SimulationSpeed> ();
+	}
+	
+	// Update is called once per frame
+	void Update () { // 10 x speed, 0.25 delta = 2.5s in frame
+		timestampUpdate += simulationSpeed.simulationSpeed * Time.deltaTime;
+		int _timestamp = timestamp + (int)timestampUpdate;
+		currentDate = epochStart.AddSeconds( _timestamp ).ToLocalTime();				// Convert unix timestamp to date/time format
+		//Debug.Log ("dtDateTime = " + months[int.Parse(currentDate.ToString("MM"))-1]+" "+currentDate.ToString("dd, yyyy"));
+	}
+	
+}
