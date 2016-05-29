26928662d666c717e3c674b809663c457ab88b2a
Cassius John-Adams <CASSIUS.JOHN-ADAMS@cbc.ca>
2016-05-29 13:35:00 -0400

    Refactoring the ScaleStates so ensure they're functioning as they should be.  Will clean up code later. #72

--- StarTographyDemo/Assets/Scripts/Setup/BuildDistanceMarkerBorder.cs
+++ StarTographyDemo/Assets/Scripts/Setup/BuildDistanceMarkerBorder.cs
@@ -5,14 +5,9 @@ public class BuildDistanceMarkerBorder : MonoBehaviour {
 
 	LineRenderer lineRenderer;
 	public Material distanceMarkerFlat;										// This is used by the fadeDistanceMarkerScript.colour variable in BuildDistanceMarkers.cs
-	public double lineRendererWidth;										// The size of the Start Width and End Width of the lines
-	double _cachedLineRendererWidth;										// To hold onto the 'real' value (without scaling) of the line renderer
 
 	void Awake () {
 		lineRenderer = gameObject.AddComponent<LineRenderer>();
-
-
-		//lineRenderer.SetWidth((float)lineRendererWidth, (float)lineRendererWidth);	// These sizes depend the DistanceMarker scale
 		distanceMarkerFlat = new Material(Resources.Load("Material/DistanceMarkerFlat") as Material);
 		distanceMarkerFlat.name = "Distance Marker Line Renderer Material";
 		lineRenderer.material = distanceMarkerFlat;							// This is used by the fadeDistanceMarkerScript.colour variable in BuildDistanceMarkers.cs
@@ -21,20 +16,8 @@ public class BuildDistanceMarkerBorder : MonoBehaviour {
 		DistanceMarkerBorder distanceMarkerBorderScript = gameObject.AddComponent<DistanceMarkerBorder>();
 		distanceMarkerBorderScript.lineRenderer = lineRenderer;
 
-		/*
-		 * TODO: Fix this, with TODO in DistanceMarkerBorder.cs, BuildDistanceMarkers.cs
-		 * The 0.00002005xxx number represents a static amount to be multiplied by whatever
-		 * line edge.
-		 */
-		_cachedLineRendererWidth = (gameObject.transform.parent.GetComponent<DistanceMarkerStates> ().distanceMarkerSize) * 0.0000000000200537613669d;
-		lineRendererWidth = _cachedLineRendererWidth;								// Cache the initial size of the lineRenderer, which is unscaled
-		lineRenderer.SetWidth((float)lineRendererWidth, (float)lineRendererWidth);	// Set the initial size of the lineRenderer
 	}
 
-	void StateUpdate(double scaleMultiplier) {
-		lineRendererWidth = _cachedLineRendererWidth / scaleMultiplier;
-		lineRenderer.SetWidth((float)lineRendererWidth, (float)lineRendererWidth);	// Set the initial size of the lineRenderer
-		print ("scaleMultiplier = " + scaleMultiplier);
-	}
+
 
 }
