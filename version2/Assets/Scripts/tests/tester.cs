53867a0c02f9f8ade409ac8c726ee49a030b2cf6
Cassius John-Adams <cassius.s.adams@gmail.com>
2016-09-25 16:18:05 -0400

    Begin setting up some of the Constant class

---
+++ version2/Assets/Scripts/tests/tester.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using Constants;
+
+public class tester : MonoBehaviour {
+	
+	public Constant sun = new Constant("m_sun","Mass of the Sun",100.05d,"kg",0.0000034d,"From my mind","si");
+	// Use this for initialization
+	void Start ()
+	{
+		Debug.Log (sun.Name);
+		Constant test = new Constant ();
+		Debug.Log (test);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
